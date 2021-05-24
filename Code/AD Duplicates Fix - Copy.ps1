d25e2896b2270f820d6af5185754117981895486
Glen Belton <gbelton@uwm.com>
2021-05-24 09:14:05 -0400

    gb test 3

---
+++ Code/AD Duplicates Fix - Copy.ps1
@@ -0,0 +1,14 @@
+﻿
+
+#$Duplicates = Get-ADComputer -Filter * | where{$_.SamAccountName -match "Duplicate"} | Select -ExpandProperty Name
+
+FOREACH ($server in $Duplicates){
+
+IF(Get-ADComputer -Filter {SAMAccountName -like "*$server*"} -ErrorAction SilentlyContinue){
+
+    Get-ADComputer -Identity $server | Select Name, SAMAccountName
+}
+ELSE {
+    Write-Warning "$Server is not present in AD"
+    }
+}
