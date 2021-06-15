8fdd2f2155fd31f7a5bbbf912a0260713a18a2f4
Glen Belton <gbelton@uwm.com>
2021-06-15 10:46:48 -0400

    Commited

--- Code/ADUsersQuery for Flori.ps1
+++ Code/ADUsersQuery for Flori.ps1
@@ -1,5 +1,5 @@
 ﻿Get-ADUser -Filter * -Properties * | where{$_.Enabled -eq "True"} | where{$_.CanonicalName -notmatch "Default SailPoint Users" -or $_.CanonicalName -notmatch "Elevated_Local_Admin" -or $_.CanonicalName -notmatch "Former Employees" -or $_.CanonicalName -notmatch "Ishbia" -or $_.CanonicalName -notmatch "MBAM" -or $_.CanonicalName -notmatch "TestingOU"} | Select -ExpandProperty DisplayName | Out-file C:\users\gbelton\Downloads\AllUWMUsers.txt
-
+tteesdfgdsfhsdt new line
 
 
 
